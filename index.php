7e40e8f53d60d89c0b56a8014ad93dbde1fb92b6
Gianni Beninati <43416301+Gbeninati@users.noreply.github.com>
2022-03-07 20:16:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php include_once("index.html"); 
-
-
-  $con = mysqli_connect("b2fkm7btnwzbsxqqtnid-mysql.services.clever-cloud.com","uwgkp87cpt4x9t57","Ij7KwdVLn75BDWy0a8HI","b2fkm7btnwzbsxqqtnid");
-  $SQL = "SELECT * FROM simulacion";
-
-  if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-  }
-
-  // Perform query
-  if ($result = mysqli_query($con, $SQL)) {
-    echo "Returned rows are: " . mysqli_num_rows($result);
-    // Free result set
-    mysqli_free_result($result);
-  }
-  mysqli_close($con);
 ?>
